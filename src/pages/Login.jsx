63e697e54cfedc2bbf67551d439d7301fa2aa737
Manvendra Rathore <manvendraofficial07@gmail.com>
2026-09-12 17:40:14 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -173,7 +173,7 @@ const Login = () => {
             <button
               type="button"
               className="forgot-password"
-              onClick={() => navigate("/forgot-password")
+              onClick={() => navigate("/forgot-password")}
             >
               Forgot Password?
             </button>
