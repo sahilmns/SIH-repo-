e30fec41c00aba639eb0d4ad5ce9bbb0df592636
Sahil Mansuri <raishmansuri039@gmail.com>
2026-09-22 15:38:02 +0530

    Route Get Started button to login

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1401,7 +1401,7 @@ function Navbar({ setSidebarOpen }) {
             <button
               type="button"
               onClick={() =>
-                navigate("/new-inspection")
+                navigate("/login")
               }
               className="
                 ml-auto
